675fab9109120ee3bdc87e149b6aaf865c90cd0d
SeAmTA <amir.taheri8002@gmail.com>
2026-07-05 08:27:23 +0330

    chore: add docker and nginx configuration

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+
 from app.routes.thread import router as thread_router
 from app.routes.process import router as process_router
 from app.routes.stream import router as stream_router
@@ -10,17 +10,14 @@ app = FastAPI(title="Parallel Processing Final Project")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-templates = Jinja2Templates(directory="app/templates")
-
 app.include_router(thread_router)
-
 app.include_router(process_router)
-
 app.include_router(stream_router)
 
+
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+def home():
+    return FileResponse(
+        "app/templates/index.html",
+        media_type="text/html"
     )
